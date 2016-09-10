c2b1c992a2d31395defd2e800831c38fb52aacf1
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-10 17:31:05 -0400

    errors when using after using getScript

--- app/assets/javascripts/find_definitons.js
+++ app/assets/javascripts/find_definitons.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   $('.find-definitions').on("click", function(){
     //This is where we capture the value
     var term = $('#term_name').val()
-    console.log()
+    $.getScript({url:"/definitions_controller/search", data: {term: term}})
   })
+
+  $('.find-definitions').on("click", "li", function(){
+    var selected_definition = $(this).text()
+    $()
+  }
+
+  )
 })
