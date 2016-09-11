307520b6d5a215f398c86e6114aa3e51df14a4e0
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-11 13:51:06 -0400

    working out routing issue

--- app/assets/javascripts/find_definitons.js
+++ app/assets/javascripts/find_definitons.js
@@ -1,25 +1,9 @@
-// $(document).ready(function() {
-//   $('.find-definitions').on("click", function(){
-//     //This is where we capture the value
-//     var term = $('#term_name').val()
-//     $.getScript({url:"/definitions_controller/search", data: {term: term}})
-//   })
-//
-//   $('.find-definitions').on("click", "li", function(){
-//     var selected_definition = $(this).text()
-//     $()
-//   }
-//
-//   )
-// })
-
-// Write Javascript here to handle the click for that button/link
 $(document).ready(function() {
 
   $(".find-definitions").on("click", function() {
-    // Get the term from the form
     var term = $("#term_name").val()
-    $.getScript({ url: "/definitions_controller/search", data: { term: term } })
+    $.getScript({ url: "/definitions_controller/search", data: {term: term} })
+    console.log()
   })
 
   $(".found-definitions").on("click", "li", function() {
--- app/controllers/definitions_controller_controller.rb
+++ app/controllers/definitions_controller_controller.rb
@@ -2,7 +2,7 @@ class DefinitionsControllerController < ApplicationController
   def search
     @term = params[:term]
 
-    @term_definitions_json = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{term}/definitions?limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    @term_definitions_json = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{URI::escape(@term)}/definitions?limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
     @definitions = @term_definitions_json.map {|definitions| definitions["text"]}
   end
