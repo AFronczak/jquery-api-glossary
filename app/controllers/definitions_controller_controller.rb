f887d82d2603f7cec71013c4941e4f0729ba9280
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-12 08:41:52 -0400

    mapping function fixed to select string, not symbol, of text

--- app/controllers/definitions_controller_controller.rb
+++ app/controllers/definitions_controller_controller.rb
@@ -7,3 +7,5 @@ class DefinitionsControllerController < ApplicationController
     @definitions = @term_definitions_json.map {|definitions| definitions["text"]}
   end
 end
+
+# HTTParty.get("http://api.wordnik.com:80/v4/word.json/Active/definitions?limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
