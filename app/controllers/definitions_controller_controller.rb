1d41168ab6fa7001b21092ce84fdc49e6230189a
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-13 09:45:56 -0400

    Update to general styling

--- app/controllers/definitions_controller_controller.rb
+++ app/controllers/definitions_controller_controller.rb
@@ -1,8 +1,10 @@
 class DefinitionsControllerController < ApplicationController
   def search
-    @term = params[:term]
+    @term = params[:term].downcase
 
-    @term_definitions_json = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{URI::escape(@term)}/definitions?limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    url = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{URI::escape(@term)}/definitions?limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+
+    @term_definitions_json = url
 
     @definitions = @term_definitions_json.map {|definitions| definitions["text"]}
   end
